334d63d0ee5cdaee177953bbd74a8a56391d4a89
oscar <oscartrent6@gmail.com>
2025-03-05 09:35:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Oscar Kiplagat'
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + '!'
+
+const upperCaseCurrentUser = currentUser.toUpperCase()
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + '!'
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser.slice[1]
+let shortGreeting = "Welcome, " + firstInitial + '!'
